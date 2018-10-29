f60e27b5dab42c8099b7e12643fdb6a436a8c385
Peng <1498284491@qq.com>
2018-10-29 23:05:15 +0800

    0

---
+++ testgit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testgit
+//
+//  Created by Daryl on 2018/10/29.
+//  Copyright © 2018 TG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testgitTests/testgitTests.m
@@ -0,0 +1,37 @@
+//
+//  testgitTests.m
+//  testgitTests
+//
+//  Created by Daryl on 2018/10/29.
+//  Copyright © 2018 TG. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testgitTests : XCTestCase
+
+@end
+
+@implementation testgitTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
